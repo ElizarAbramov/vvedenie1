eeeb811dac4a938cf1201a4a7e6f1e57ea100c57
Elizar Abramov <nezha2077@mail.ru>
2021-09-27 19:29:33 +0700

    Main.java added

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentbalance = 2_000_000_000;
+        int transferamount = 500_000_000;
+        int total = currentbalance * transferamount;
+        System.out.println(total);
+    }
+}
