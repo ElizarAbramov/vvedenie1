3e935ead560121bf81c445cfd0b2661ef1165dbb
Elizar Abramov <nezha2077@mail.ru>
2021-09-28 15:37:15 +0700

    "Main.java" changed

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int currentbalance = 2_000_000_000;
         int transferamount = 500_000_000;
-        int total = currentbalance * transferamount;
+        int total = currentbalance + transferamount;
         System.out.println(total);
     }
 }
